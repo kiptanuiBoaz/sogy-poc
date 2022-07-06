ee097cf852c60998ce35b846029e514aec7331a1
Boaz <boaserem022@gmail.com>
2022-07-06 18:02:21 +0300

    merge release

---
+++ build/precache-manifest.dc870786235f718e970aecbce6385566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb6955ef70d04770764d",
+    "url": "/static/js/main.fb6955ef.chunk.js"
+  },
+  {
+    "revision": "4d6c10c9e12a07c4b54e",
+    "url": "/static/css/1.35d43a16.chunk.css"
+  },
+  {
+    "revision": "4d6c10c9e12a07c4b54e",
+    "url": "/static/js/1.4d6c10c9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05ce7a0ef6a883be55d28014bb3536a2",
+    "url": "/index.html"
+  }
+];
