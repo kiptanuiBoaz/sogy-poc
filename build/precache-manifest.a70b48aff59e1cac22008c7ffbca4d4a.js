afafc1b80b3943d016093b4ffc92d85e822fa056
Boaz <boaserem022@gmail.com>
2022-07-12 12:16:27 +0300

    git commit

---
+++ build/precache-manifest.a70b48aff59e1cac22008c7ffbca4d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aded4a70ebb6d5d705ba",
+    "url": "/static/js/main.aded4a70.chunk.js"
+  },
+  {
+    "revision": "aff4d96f5a3fd5cf3fe7",
+    "url": "/static/css/1.cf224ff5.chunk.css"
+  },
+  {
+    "revision": "aff4d96f5a3fd5cf3fe7",
+    "url": "/static/js/1.aff4d96f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfdbfe1d766dfbea7e7c3a536c75b6f7",
+    "url": "/index.html"
+  }
+];
