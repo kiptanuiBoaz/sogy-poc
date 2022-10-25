a5808fc4ea2cbd7728c6ae2ed3ff0130bc1a7e72
boaz serem <boaserem022@gmail.com>
2022-10-25 18:09:59 +0300

    removed bug

---
+++ build/precache-manifest.04e8826623e5b0623eb51306db177179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18ce6cd49c60aa2e4f20",
+    "url": "/static/css/main.fb67e58b.chunk.css"
+  },
+  {
+    "revision": "18ce6cd49c60aa2e4f20",
+    "url": "/static/js/main.18ce6cd4.chunk.js"
+  },
+  {
+    "revision": "b3495ec5aca2f9cd74e8",
+    "url": "/static/css/1.cd7a4a50.chunk.css"
+  },
+  {
+    "revision": "b3495ec5aca2f9cd74e8",
+    "url": "/static/js/1.b3495ec5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dee3b37ccb5f72b0f40bce590624268c",
+    "url": "/index.html"
+  }
+];
