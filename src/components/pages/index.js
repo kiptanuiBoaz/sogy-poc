5aadd3f69241facd10893d13e7bc4344845bcb79
_Allan <allankipchumba76@gmail.com>
2022-10-26 19:13:10 +0300

    fixed typo in index.js file in components directory

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,11 +1,22 @@
-export {Welcome} from "./welcome/Welcome";
-export {Footer}  from "./footer/Footer";
-export {Rooms } from "./rooms/Rooms";
-export {Reserve}  from "./reserve/Reserve";
-export {Photos}  from "./photosPage/Photos";
-export {Events}  from "./events/EventContainer";
-export {Menu}  from "./menu/Menu";
-export {ContactUs}  from "./contactUs/ContactUs";
-export {NavBar} from "./navbar/NavBar";
-export {Homepage}  from "./homepage/Homepage";
-export {Reservations} from "./bookRoom/Reservations";
+export { Welcome }
+from "./welcome/Welcome";
+export { Footer }
+from "./footer/Footer";
+export { Rooms }
+from "./rooms/Rooms";
+export { Reserve }
+from "./reserve/Reserve";
+export { Photos }
+from "./photosPage/Photos";
+export { Events }
+from "./events/EventContainer";
+export { Menu }
+from "./menu/Menu";
+export { ContactUs }
+from "./contactUs/ContactUs";
+export { NavBar }
+from "./navbar/Navbar";
+export { Homepage }
+from "./homepage/Homepage";
+export { Reservations }
+from "./bookRoom/Reservations";
