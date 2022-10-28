cab5061047d80ccefd8bf372e574b996ff187f4f
boaz serem <boaserem022@gmail.com>
2022-10-28 21:11:28 +0300

    conf

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReserveButton from "./ReserveButton";
-import Text from "./Text";
 import Typed from "react-typed";
 import Jump from 'react-reveal/Jump';
 import "./home_page_styles/homePage.css";
@@ -11,7 +10,6 @@ export const  Homepage = ()=> {
     <div  className= "homepage" name="/">
     
       <h2 className="welcomeText" style={{paddingTop:"130px"}}>WELCOME TO  5 <span style={{color:"#ffba5a"}}>&#9733;</span> &nbsp; HOTEL</h2>
-      <Text text="We make the most memorabe stay here"/>
       <Typed 
         className="typing-effect"
         strings ={[
@@ -25,7 +23,6 @@ export const  Homepage = ()=> {
         loop 
       />
       
-      <Text className="fourth-text" text = "Eat well rest, well with us!"/>
 
       <Jump delay= {1500} count={20}>
         {<ReserveButton text= {"Reserve Now"} />}
