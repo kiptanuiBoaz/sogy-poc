17978f5bcac95859839a02424c4fa8e3e707625e
boaz serem <boaserem022@gmail.com>
2022-10-31 14:55:04 +0300

    dev dep

---
+++ build/precache-manifest.3da4ace2c22067b88c61345972bee67d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b0b7c48de0b9b55302d",
+    "url": "/kiptanuiBoaz/sogy-poc/static/css/main.fe3e95a0.chunk.css"
+  },
+  {
+    "revision": "2b0b7c48de0b9b55302d",
+    "url": "/kiptanuiBoaz/sogy-poc/static/js/main.ac806dd3.chunk.js"
+  },
+  {
+    "revision": "872632df4de8443c03a8",
+    "url": "/kiptanuiBoaz/sogy-poc/static/js/runtime~main.4ed343c3.js"
+  },
+  {
+    "revision": "0629c1bec2e7007a24c4",
+    "url": "/kiptanuiBoaz/sogy-poc/static/css/2.c0e226ae.chunk.css"
+  },
+  {
+    "revision": "0629c1bec2e7007a24c4",
+    "url": "/kiptanuiBoaz/sogy-poc/static/js/2.6b47b1cd.chunk.js"
+  },
+  {
+    "revision": "1477d811e940ad1913ff1e434eda6027",
+    "url": "/kiptanuiBoaz/sogy-poc/index.html"
+  }
+];
