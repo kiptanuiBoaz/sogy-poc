1ac4159fb46bf2ff078c45a3154a3d7157798577
boaz serem <boaserem022@gmail.com>
2022-11-23 13:52:18 +0300

    added pics

--- src/components/assets/photoList.js
+++ src/components/assets/photoList.js
@@ -1,49 +1,49 @@
 const photoList = [
     {
         id:1,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-1.jpg.pagespeed.ic.9u6JJzRS7-.webp",
+        image:"https://images.unsplash.com/photo-1669058611053-ff24fb0f1c33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"bed lamp and pillow",
         caption:"Well furnished"
     },
     
     {
         id:2,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-2.jpg.pagespeed.ic.-iGvF6CrIC.webp",
+        image:"https://images.unsplash.com/photo-1669051759318-e3f68b839d1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0M3x8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"bed and sunshine window",
         caption:"All glass windows"
     },
 
     {
         id:3,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-5.jpg.pagespeed.ic.XqFfDt9nFs.webp",
+        image:"https://images.unsplash.com/photo-1661961110144-12ac85918e40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"white room and white bed",
         caption:"All white"
     },
 
     {   
         id:4,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-6.jpg.pagespeed.ic.0vGJrHw61J.webp",
+        image:"https://plus.unsplash.com/premium_photo-1663946191754-656f3eef9f76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"valley view",
         caption:"Valley view"
     },
 
     {
         id:5,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-7.jpg.pagespeed.ic.rqVXoHrULE.webp",
+        image:"https://images.unsplash.com/photo-1669046635809-b682a850ea5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
         alt:"beach view",
         caption:"Beach view"
     },
 
     {
         id:6,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-3.jpg.pagespeed.ic.Jo-05SSikC.webp",
+        image:"https://images.unsplash.com/photo-1669064206623-10e3b30231c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"bedlamp and bed",
         caption:"Comfy setup"
     },
 
     {
         id:7,
-        image:"https://preview.colorlib.com/theme/sogo/images/xslider-4.jpg.pagespeed.ic.amA4Wbo-tp.webp",
+        image:"https://images.unsplash.com/photo-1669111957777-9cdf871e7be5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4MHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
         alt:"living room",
         caption:"living room"
     }
--- src/components/pages/sign-up/SignUp.jsx
+++ src/components/pages/sign-up/SignUp.jsx
@@ -1,192 +1,192 @@
-import React, {  useState } from "react";
-import "./new.scss";
-import Sidebar from "../sidebar/Sidebar";
-// ./src/components/pages/sign-up/SignUp
+// import React, {  useState } from "react";
+// import "./new.scss";
+// import Sidebar from "../sidebar/Sidebar";
+// // ./src/components/pages/sign-up/SignUp
 // import Navbar from "../../components/navbar/Navbar";
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-// import {storageRef ,setDoc, serverTimestamp, doc } from "firebase/firestore";
-// import { auth, db, storage } from "../../firebase";
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import {useNavigate} from "react-router-dom";
+// import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+// // import {storageRef ,setDoc, serverTimestamp, doc } from "firebase/firestore";
+// // import { auth, db, storage } from "../../firebase";
+// // import { createUserWithEmailAndPassword } from "firebase/auth";
+// // import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+// import {useNavigate} from "react-router-dom";
 
-export const SignUp = () => {
-    const title="title";
-    const inputs=[
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",
-        },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",
-        },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",        },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",        },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",       
-         },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",       
-         },
-        { 
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",
-            },
-        {
-            placeholder:"John Doe",
-            label:"label",
-            id:1,
-            type:"text",        },]
-        const [file, setFile] = useState("");
-        const [data, setData] = useState({});
-        // const [perc, setPerce] = useState(null);
-        const navigate = useNavigate();
+// export const SignUp = () => {
+//     const title="title";
+//     const inputs=[
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",
+//         },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",
+//         },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",        },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",        },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",       
+//          },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",       
+//          },
+//         { 
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",
+//             },
+//         {
+//             placeholder:"John Doe",
+//             label:"label",
+//             id:1,
+//             type:"text",        },]
+//         const [file, setFile] = useState("");
+//         const [data, setData] = useState({});
+//         // const [perc, setPerce] = useState(null);
+//         const navigate = useNavigate();
 
-//   useEffect(() => {
-//     const uploadFile = () => {
+// //   useEffect(() => {
+// //     const uploadFile = () => {
 
-//     //   const name = new Date().getTime() + file.name;
-//     // //   const storageRef = ref(storage, name);
-//     //   const uploadTask = uploadBytesResumable(storageRef, file);
+// //     //   const name = new Date().getTime() + file.name;
+// //     // //   const storageRef = ref(storage, name);
+// //     //   const uploadTask = uploadBytesResumable(storageRef, file);
 
-//       // Register three observers:
-//       // 1. 'state_changed' observer, called any time the state changes
-//       // 2. Error observer, called on failure
-//       // 3. Completion observer, called on successful completion
-//     //   uploadTask.on('state_changed',
-//     //     (snapshot) => {
-//     //       // Observe state change events such as progress, pause, and resume
-//     //       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-//     //       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//     //       setPerce(progress);
-//     //       console.log('Upload is ' + progress + '% done');
-//     //       switch (snapshot.state) {
-//     //         case 'paused':
-//     //           console.log('Upload is paused');
-//     //           break;
-//     //         case 'running':
-//     //           console.log('Upload is running');
-//     //           break;
-//     //         default:
-//     //           break;
-//     //       }
-//     //     },
-//     //     (error) => {
-//     //       // Handle unsuccessful uploads
-//     //       console.log(error);
-//     //     },
-//     //     () => {
-//     //       // Handle successful uploads on complete
-//     //       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-//     //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-//     //         setData((prev) => ({ ...prev, img: downloadURL }))
-//     //       });
-//     //     }
-//     //   );
+// //       // Register three observers:
+// //       // 1. 'state_changed' observer, called any time the state changes
+// //       // 2. Error observer, called on failure
+// //       // 3. Completion observer, called on successful completion
+// //     //   uploadTask.on('state_changed',
+// //     //     (snapshot) => {
+// //     //       // Observe state change events such as progress, pause, and resume
+// //     //       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+// //     //       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+// //     //       setPerce(progress);
+// //     //       console.log('Upload is ' + progress + '% done');
+// //     //       switch (snapshot.state) {
+// //     //         case 'paused':
+// //     //           console.log('Upload is paused');
+// //     //           break;
+// //     //         case 'running':
+// //     //           console.log('Upload is running');
+// //     //           break;
+// //     //         default:
+// //     //           break;
+// //     //       }
+// //     //     },
+// //     //     (error) => {
+// //     //       // Handle unsuccessful uploads
+// //     //       console.log(error);
+// //     //     },
+// //     //     () => {
+// //     //       // Handle successful uploads on complete
+// //     //       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+// //     //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+// //     //         setData((prev) => ({ ...prev, img: downloadURL }))
+// //     //       });
+// //     //     }
+// //     //   );
 
 
-//     }
-//     file && uploadFile();
-//   }, [file])
+// //     }
+// //     file && uploadFile();
+// //   }, [file])
 
-//   const handleAdd = async (e) => {
-//     e.preventDefault();
+// //   const handleAdd = async (e) => {
+// //     e.preventDefault();
 
-//     try {
-//       const res = await createUserWithEmailAndPassword(
-//         auth,
-//         data.email,
-//         data.password
-//       );
+// //     try {
+// //       const res = await createUserWithEmailAndPassword(
+// //         auth,
+// //         data.email,
+// //         data.password
+// //       );
 
-//       await setDoc(doc(db, "users", res.user.uid), {
-//         ...data,
-//         timeStamp: serverTimestamp()
-//       });
-//       navigate(-1)
-//       console.log(res);
-//     } catch (error) {
-//       console.log(error);
-//     }
+// //       await setDoc(doc(db, "users", res.user.uid), {
+// //         ...data,
+// //         timeStamp: serverTimestamp()
+// //       });
+// //       navigate(-1)
+// //       console.log(res);
+// //     } catch (error) {
+// //       console.log(error);
+// //     }
 
+// //   }
+
+//   const handleInput = (e) => {
+
+//     setData({ ...data, [e.target.id]: e.target.value })
 //   }
 
-  const handleInput = (e) => {
-
-    setData({ ...data, [e.target.id]: e.target.value })
-  }
 
 
-
-  return (
-    <div className="new">
-      <Sidebar />
-      <div className="newContainer">
+//   return (
+//     <div className="new">
+//       <Sidebar />
+//       <div className="newContainer">
         
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            />
-          </div>
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
-                />
-              </div>
+//         <div className="top">
+//           <h1>{title}</h1>
+//         </div>
+//         <div className="bottom">
+//           <div className="left">
+//             <img
+//               src={
+//                 file
+//                   ? URL.createObjectURL(file)
+//                   : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+//               }
+//               alt=""
+//             />
+//           </div>
+//           <div className="right">
+//             <form>
+//               <div className="formInput">
+//                 <label htmlFor="file">
+//                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
+//                 </label>
+//                 <input
+//                   type="file"
+//                   id="file"
+//                   onChange={(e) => setFile(e.target.files[0])}
+//                   style={{ display: "none" }}
+//                 />
+//               </div>
 
-              {inputs.map(({ placeholder, label, id, type }) => (
-                <div className="formInput" key={id}>
-                  <label>{label}</label>
-                  <input id={id} onChange={handleInput} type={type} placeholder={placeholder} />
-                </div>
-              ))}
-              <button 
-              // dissbled={perc !==null &&  perc < 100} 
-            //   onClick={handleAdd}
-              >Send</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//               {inputs.map(({ placeholder, label, id, type }) => (
+//                 <div className="formInput" key={id}>
+//                   <label>{label}</label>
+//                   <input id={id} onChange={handleInput} type={type} placeholder={placeholder} />
+//                 </div>
+//               ))}
+//               <button 
+//               // dissbled={perc !==null &&  perc < 100} 
+//             //   onClick={handleAdd}
+//               >Send</button>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
