68021e68ea40a545fadc06909edd78ff746b5650
kiptanui boaz <boaserem022@gmail.com>
2023-01-13 14:58:42 +0300

    updated footer links

--- src/components/pages/footer/Footer.jsx
+++ src/components/pages/footer/Footer.jsx
@@ -10,21 +10,20 @@ import "./footer.css"
 
 export const  Footer = () => {
     return (
-      <section className="footer" tooltip="This is the developer info row">
+      <section className="footer" tooltip="This is the developer info ">
         <div>
-          <p>Copyright ©{new Date().getFullYear()} || Developed by Elvis Mariga </p>
-          <img src="https://pps.whatsapp.net/v/t61.24694-24/310564933_855449148976870_4419258625965570890_n.jpg?ccb=11-4&oh=01_AdRq9ULj3SNhQtIAL-D6wqSY6bOb6B75x3WqmgezAezX6A&oe=638B1D99" alt="avatar" />
-          <a rel="noreferrer" target={"_blank"} href="mailto:elvmariga@gmail.com
-"> <MdEmail/> &nbsp; elvmariga@gmail.com
+          <p>Copyright ©{new Date().getFullYear()} || Developed by Kiptanui Boaz </p>
+          <img src="https://avatars.githubusercontent.com/u/84452739?v=4" alt="avatar" />
+          <a rel="noreferrer"   target={"_blank"} href=" boaserem022@gmail.com"> <MdEmail/> &nbsp; boaserem022@gmail.com
 </a>
-          <a rel="noreferrer" target={"_blank"} href="tel:+254 719 311036"> <FaPhoneAlt/> &nbsp; +254 719 311036</a>
+          <a rel="noreferrer"   target={"_blank"} href="tel:+254705935133"> <FaPhoneAlt/> &nbsp; +254705935133</a>
         </div>
         
         <div className="icons">
           <a rel="noreferrer" target={"_blank"} href="https://web.facebook.com"><BsFacebook/></a> 
-          <a rel="noreferrer" target={"_blank"} href="https://twitter.com/ElvisMariga1"><ImTwitter/></a>
-          <a rel="noreferrer" target={"_blank"} href="https://github.com/elvmariga"><BsGithub/></a>
-          <a rel="noreferrer" target={"_blank"} href="https://www.linkedin.com"><ImLinkedin/></a>
+          <a rel="noreferrer" target={"_blank"} href="https://twitter.com/k_boazo"><ImTwitter/></a>
+          <a rel="noreferrer" target={"_blank"} href="https://github.com/kiptanuiBoaz"><BsGithub/></a>
+          <a rel="noreferrer" target={"_blank"} href="https://www.linkedin.com/in/kiptanui-boaz-466154217/"><ImLinkedin/></a>
         </div>
        
       </section>
